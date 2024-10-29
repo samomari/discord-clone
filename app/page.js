9ff095dc796ebc46a3d1b02b97bd52f418b70fe1
MarSam <marius.sam98@gmail.com>
2024-10-29 14:27:49 +0200

    added shadcn

--- app/page.js
+++ app/page.js
@@ -1,6 +1,6 @@
 import Image from "next/image";
 
-export default function Home() {
+export default function Page() {
   return (
     <div className="grid items-center justify-items-center">
       <h1>UNDER WORK</h1>
