1899254021dee9ddf6b9d5be366e51fa21a8b28e
samomari <34354282+samomari@users.noreply.github.com>
2024-10-31 14:55:46 +0200

    Revert "added create a server modal with image uploading"

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -1,27 +1,11 @@
-'use client'
-import InitialModal from "@/components/modals/initial-modal";
 import { ModeToggle } from "@/components/mode-toggle";
 import { UserButton } from "@clerk/nextjs";
-import { useState } from "react";
 
 export default function Page() {
-  const [isModalOpen, setIsModalOpen] = useState(false);
-  const openModal = () => {
-    setIsModalOpen(true);
-  };
-
-  const closeModal = () => {
-    setIsModalOpen(false);
-  };
   return (
     <div>
       <UserButton />
       <ModeToggle />
-      <button onClick={openModal} className="btn-primary">
-        Create a server
-      </button>
-      
-      {isModalOpen && <InitialModal onClose={closeModal} />}
     </div>
   );
 }
