3f984acde041cdf0331e567eabd56d5d78a1d983
MarSam <marius.sam98@gmail.com>
2024-11-13 13:09:52 +0200

    Add server search

--- components/server/server-sidebar.tsx
+++ components/server/server-sidebar.tsx
@@ -1,14 +1,28 @@
 import { currentProfile } from "@/lib/current-profile";
 import { redirect } from "next/navigation";
 import { db } from "@/db/db";
-import { server, member, channel, profile, ChannelTypeEnum } from "@/db/schema";
+import { server, member, channel, profile } from "@/db/schema";
 import { eq, and, asc } from "drizzle-orm";
 import { ServerHeader } from "./server-header";
 import { ServerWithMembersWithProfiles } from "@/types";
+import { ScrollArea } from "@/components/ui/scroll-area";
+import { ServerSearch } from "./server-search";
+import { Hash, Mic, ShieldAlert, ShieldCheck } from "lucide-react";
 
 interface ServerSidebarProps {
   serverId: string;
 }
+
+const iconMap = {
+  "TEXT": <Hash className="mr-2 h-4 w-4"/>,
+  "VOICE": <Mic className="mr-2 h-4 w-4"/>
+};
+
+const roleIconMap = {
+  "GUEST": null,
+  "MODERATOR": <ShieldCheck className="h-4 w-4 mr-2 text-indigo-500" />,
+  "ADMIN": <ShieldAlert className="h-4 w-4 mr-2 text-rose-500"/>
+};
 
 export const ServerSidebar = async ({
   serverId
@@ -35,7 +49,7 @@ export const ServerSidebar = async ({
   const serverItem = serverData[0];
 
   const membersMap = new Map();
-  const channelsArray = [];
+  const channelsMap = new Map();
   serverData.forEach((item) => {
     if (item.members?.id) {
       membersMap.set(item.members.id, {
@@ -46,18 +60,19 @@ export const ServerSidebar = async ({
         profile: item.profiles,
       });
     }
-    if (item.channels?.id) {
-      channelsArray.push(item.channels);
+    if (item.channels?.id && !channelsMap.has(item.channels.id)) {
+      channelsMap.set(item.channels.id, item.channels);
     }
   });
 
   const membersArray = Array.from(membersMap.values());
+  const channelsArray = Array.from(channelsMap.values());
 
   const textChannels = channelsArray.filter(
-    (channel) => channel.type === ChannelTypeEnum["TEXT"]);
-
+    (channel) => channel.type === "TEXT");
+  
   const voiceChannels = channelsArray.filter(
-    (channel) => channel.type === ChannelTypeEnum["VOICE"]);
+    (channel) => channel.type === "VOICE");
 
   const filteredMembers = membersArray.filter(
     (member) => member.profileId !== curProfile.id);
@@ -83,6 +98,41 @@ export const ServerSidebar = async ({
         server={reshapedServerItem}
         role={role}
       />
+      <ScrollArea className="flex-1 px-3">
+        <div className="mt-2">
+          <ServerSearch 
+            data={[
+              {
+                label: "Text Channels",
+                type: "channel",
+                data: textChannels?.map((channel) => ({
+                  id: channel.id,
+                  name: channel.name,
+                  icon: iconMap[channel.type],
+                }))
+              },
+              {
+                label: "Voice Channels",
+                type: "channel",
+                data: voiceChannels?.map((channel) => ({
+                  id: channel.id,
+                  name: channel.name,
+                  icon: iconMap[channel.type],
+                }))
+              },
+              {
+                label: "Members",
+                type: "member",
+                data: filteredMembers?.map((member) => ({
+                  id: member.id,
+                  name: member.profile.name,
+                  icon: roleIconMap[member.role],
+                }))
+              }
+            ]}
+          />
+        </div>
+      </ScrollArea>
     </div>
   );
 }
