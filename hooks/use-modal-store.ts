542ca23428925f7c5c0b2f998af959fb4579de3d
MarSam <marius.sam98@gmail.com>
2024-11-21 15:23:04 +0200

    Add delete message

--- hooks/use-modal-store.ts
+++ hooks/use-modal-store.ts
@@ -2,7 +2,7 @@ import { create } from 'zustand';
 import { SelectChannel, SelectServer } from "@/db/schema"
 import { ChannelType } from '@/types';
 
-export type ModalType = 'createServer' | 'invite' | 'editServer' | 'members' | 'createChannel' | 'leaveServer' | 'deleteServer' | 'deleteChannel' | 'editChannel' | 'messageFile';
+export type ModalType = 'createServer' | 'invite' | 'editServer' | 'members' | 'createChannel' | 'leaveServer' | 'deleteServer' | 'deleteChannel' | 'editChannel' | 'messageFile' | 'deleteMessage';
 
 interface ModalData {
   server?: SelectServer;
