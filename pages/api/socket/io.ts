96620ebe3d7c4fe6b5a80733092d8ac8eab9b8fa
MarSam <marius.sam98@gmail.com>
2024-11-28 13:22:58 +0200

    Fix socket.io server config

--- pages/api/socket/io.ts
+++ pages/api/socket/io.ts
@@ -3,24 +3,38 @@ import { NextApiRequest } from "next";
 import { Server as ServerIO } from "socket.io";
 import { NextApiResponseServerIO } from "@/types";
 
-export const config = {
-  api: {
-    bodyParser: false,
-  },
-};
-
-const ioHandler = (req: NextApiRequest, res: NextApiResponseServerIO) => {
+export default function handler (req: NextApiRequest, res: NextApiResponseServerIO) {
   if (!res.socket.server.io) {
-    const path = "/api/socket/io";
+    console.log('Starting Socket.IO server...');
+
     const httpServer: NetServer = res.socket.server as any;
     const io = new ServerIO(httpServer, {
-      path: path,
-      addTrailingSlash: false,
+      path: '/api/socket/io',
+      cors: {
+        origin: 'http://localhost:3000',
+        methods: ['GET', 'POST'],
+        allowedHeaders: ['Content-Type'],
+  credentials: true,
+      },
     });
+
+    io.on('connection', (socket) => {
+      console.log('A user connected: ', socket.id);
+
+      socket.on('message', (message) => {
+        console.log('Received message: ', message);
+        io.emit('message', message);
+      });
+
+      socket.on('disconnect', () => {
+        console.log('A user disconnected: ', socket.id);
+      });
+    });
+
     res.socket.server.io = io;
+  } else {
+    console.log('Socket.IO server already running.');
   }
 
   res.end();
-};
-
-export default ioHandler;
+}
