55a568ee0f028e21650c5de73427f216fd3620dc
MarSam <marius.sam98@gmail.com>
2024-11-28 13:21:38 +0200

    Fix naming typo

--- components/providers/modal-provider.tsx
+++ components/providers/modal-provider.tsx
@@ -11,7 +11,7 @@ import { DeleteServerModal } from "@/components/modals/delete-server-modal";
 import { DeleteChannelModal } from "@/components/modals/delete-channel-modal";
 import { EditChannelModal } from "@/components/modals/edit-channel-modal";
 import { MessageFileModal } from "@/components/modals/message-file-modal";
-import { DeleteMessageModal } from "@/components/modals/delete-message-modal copy";
+import { DeleteMessageModal } from "@/components/modals/delete-message-modal";
 
 export const ModalProvider = () => {
   const [ isMounted, setIsMounted ] = useState(false);
